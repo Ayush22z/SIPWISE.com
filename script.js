4fa5209ddeb1c6371a06a42347fb6ec0ddedd1d5
Ayush22z <ayushbang22@gmail.com>
2025-06-28 15:14:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ function calculateSIP() {
   const years = parseFloat(document.getElementById('years').value);
   const cagr = parseFloat(document.getElementById('cagr').value);
 
+  if (isNaN(sip) || isNaN(years) || isNaN(cagr)) {
+    document.getElementById('result').innerHTML = "❗ Please enter all values.";
+    return;
+  }
+
   const months = years * 12;
   const monthlyRate = cagr / 100 / 12;
 
-  let futureValue = sip * (((Math.pow(1 + monthlyRate, months)) - 1) / monthlyRate) * (1 + monthlyRate);
+  const futureValue = sip * (((Math.pow(1 + monthlyRate, months)) - 1) / monthlyRate) * (1 + monthlyRate);
 
   document.getElementById('result').innerHTML =
     `<h3>Expected Value: ₹${futureValue.toFixed(2)}</h3>`;
